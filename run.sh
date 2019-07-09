8cea7b88e127d0b130282361bdc63b06ac5dd87d
Henning Andreseck <henning.andreseck@eves-co.de>
2019-07-09 10:34:52 +0200

    unifi-poller integriert

--- run.sh
+++ run.sh
@@ -1,13 +1,51 @@
 #!/bin/bash
+echo "##########################################################"
+echo "Bring up Loxone Reporting Stack: Grafana, InfluxDB, unifi-poller and NodeRed incl. loxone and influxdb"
+echo
+echo "Please make sure, to edit unifi-poller/up.conf!"
+echo
+echo "InfluxDB is available in NodeRed as 'influxdb'"
+echo 
+echo "##########################################################"
+read -p "Press enter to continue"
 
 docker-compose up -d
 
 echo "Grafana: http://127.0.0.1:3000 - admin/admin"
+echo "Nodered: http://127.0.0.1:1880"
+
+echo "Check and create loxone and unifi Database"
+curl -G http://localhost:8086/query?pretty=true --data-urlencode "db=glances" --data-urlencode "q=SHOW DATABASES" | grep loxone > /dev/null
+unifi=$?
+echo $unifi
+if [ $unifi -ne "0" ]; then
+	echo "create loxone DB"
+	curl -XPOST 'http://localhost:8086/query' --data-urlencode 'q=CREATE DATABASE loxone'
+fi
+curl -G http://localhost:8086/query?pretty=true --data-urlencode "db=glances" --data-urlencode "q=SHOW DATABASES" | grep unifi > /dev/null
+loxone=$?
+echo $loxone
+if [ $loxone -ne "0" ]; then
+	echo "create unifi DB"
+	curl -XPOST 'http://localhost:8086/query' --data-urlencode 'q=CREATE DATABASE unifi'
+fi
 
 echo
+echo "##########################################################"
 echo "Current database list"
 curl -G http://localhost:8086/query?pretty=true --data-urlencode "db=glances" --data-urlencode "q=SHOW DATABASES"
+echo
+echo "##########################################################"
 
+echo
+echo
+echo
+
+echo "##########################################################"
+echo
+echo
+echo "Grafana: http://127.0.0.1:3000 - admin/admin"
+echo "Nodered: http://127.0.0.1:1880"
 echo
 echo "Create a new database ?"
 echo "curl -XPOST 'http://localhost:8086/query' --data-urlencode 'q=CREATE DATABASE mydb'"
